71b8368a2f9b7375184740b12e77d1f599d7c273
Alexander Schischkin <schischkin2@yandex.com>
2026-06-23 15:59:57 +0200

    прописываем настройки в settings.py

--- config/settings.py
+++ config/settings.py
@@ -94,3 +94,7 @@ STATIC_URL = 'static/'
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#Настройки для медиа
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
