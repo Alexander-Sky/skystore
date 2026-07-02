08dbfdf19c4be1ad02f51f0cbe1545c6c691a3c1
Alexander Schischkin <schischkin2@yandex.com>
2026-07-02 16:37:51 +0200

    Обновил контролер на(CBV)

--- catalog/views.py
+++ catalog/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render, get_object_or_404
 from .models import Product
 from django.views.generic import ListView
 from django.views.generic import TemplateView
+from django.views.generic import DetailView
 
 
 class HomeView(ListView):
@@ -18,6 +19,7 @@ class ContactsView(TemplateView):
     template_name = 'catalog/contacts.html'
 
 
-def product_detail(request, pk):
-    product = get_object_or_404(Product, pk=pk)
-    return render(request, 'catalog/product_detail.html', {'product': product})
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'catalog/product_detail.html'
+    context_object_name = 'product'
