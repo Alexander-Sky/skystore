fbb6267e39102661d841692bc6210bc7e089f723
Alexander Schischkin <schischkin2@yandex.com>
2026-07-25 11:57:34 +0200

    обновил url.py

--- catalog/urls.py
+++ catalog/urls.py
@@ -4,7 +4,9 @@ from . import views
 app_name = 'catalog'
 
 urlpatterns = [
-    path('', views.HomeView.as_view(), name='home'),
-    path('contacts/', views.ContactsView.as_view(), name='contacts'),
-    path('products/<int:pk>/', views.ProductDetailView.as_view(), name='product_detail'),
+    path('', views.ProductListView.as_view(), name='product_list'),
+    path('<int:pk>/', views.ProductDetailView.as_view(), name='product_detail'),
+    path('create/', views.ProductCreateView.as_view(), name='product_create'),
+    path('<int:pk>/update/', views.ProductUpdateView.as_view(), name='product_update'),
+    path('<int:pk>/delete/', views.ProductDeleteView.as_view(), name='product_delete'),
 ]
