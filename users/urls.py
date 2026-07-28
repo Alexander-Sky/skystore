4d7b771ecaaed7bab7c7aa146cc02ee550d8f65e
Alexander Schischkin <schischkin2@yandex.com>
2026-07-28 21:11:28 +0200

    исправил настройки маршрутизации

--- users/urls.py
+++ users/urls.py
@@ -1,13 +1,10 @@
-from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
+from django.urls import path
+from . import views
+
+app_name = 'users'
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('catalog.urls')),
-    path('users/', include('users.urls')),    # <-- HIER EINFÜGEN!
+    path('register/', views.register, name='register'),
+    path('login/', views.user_login, name='login'),
+    path('logout/', views.user_logout, name='logout'),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
