951a9c9286b9857b0860cdb72db69f69259487ef
Alexander Schischkin <schischkin2@yandex.com>
2026-07-28 19:06:47 +0200

    Создаём Модель пользователя

--- users/models.py
+++ users/models.py
@@ -1,3 +1,18 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    email = models.EmailField(unique=True, verbose_name="Электронная почта")
+    phone = models.CharField(max_length=20, blank=True, null=True, verbose_name="Телефон")
+    country = models.CharField(max_length=100, blank=True, null=True, verbose_name="Страна")
+    avatar = models.ImageField(upload_to='avatars/', blank=True, null=True, verbose_name="Аватар")
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+
+    def __str__(self):
+        return self.email
